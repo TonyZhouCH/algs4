d2dcffc5e220114cda3e734b97aefea23041fa8a
TonyZhouCH <543091895@qq.com>
2017-12-05 22:10:46 +0800

    the end of unit2

---
+++ unit-2/ElementarySorts/Shell.java
@@ -0,0 +1,28 @@
+public class Shell {
+	public static void sort(Comparable[] a) {
+		int N = a.length;
+		int h = 1;
+
+		while (h < N / 3) {
+			h = 3 * h + 1;
+		}
+
+		while(h >= 1) {
+			for (int i = h; i < N; i++) {
+				if (int j = i; j >= h && less(a[j], a[j-h]); j -= h) {
+					exch(a, j, j-h);
+				}
+			}
+
+			h = h / 3;
+		}
+	}
+
+	private static boolean less(Comparable v, Comparable w) {
+		/* as before */
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		/* as before */
+	}
+}
